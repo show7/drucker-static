13dd37bb4dcbf1177ba968676cb96f86ecebfc34
lance <451230805@qq.com>
2018-05-04 20:42:20 +0800

    添加列子

---
+++ src/components/common/example/example.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="example">
+      <p>{{exampleMsg}}</p>
+      <input v-model="exampleMsg" @input="emitData(exampleMsg)" placeholder="edit me">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'example',
+  props: ['msg'],
+  data () {
+    return {
+      exampleMsg: this.msg
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    emitData (value) {
+      this.$emit('getData', value)
+    }
+  }
+}
+</script>
+
+<style lang="less">
+  @import "example.less";
+</style>
---
+++ src/components/helloWorld/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <el-button type="text" @click="outerVisible = true">点击打开外层 Dialog</el-button>
+
+    <el-dialog title="外层 Dialog" :visible.sync="outerVisible">
+      <el-dialog
+        width="30%"
+        title="内层 Dialog"
+        :visible.sync="innerVisible"
+        append-to-body>
+      </el-dialog>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="outerVisible = false">取 消</el-button>
+        <el-button type="primary" @click="innerVisible = true">打开内层 Dialog</el-button>
+      </div>
+    </el-dialog>
+
+    <example :msg="msg" @getData="get"></example>
+    <p >父组件：{{msg}}</p>
+  </div>
+</template>
+
+<script>
+import example from '@/components/common/example/example';
+export default {
+  name: 'helloWorld',
+  components: { example },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      outerVisible: false,
+      innerVisible: false
+    }
+  },
+  methods: {
+    get (string) {
+      this.msg = string;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less">
+@import "HelloWorld.less";
+</style>
