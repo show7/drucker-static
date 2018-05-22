948841589bbf273423b72bf5494ca60c1c27a88b
lance <451230805@qq.com>
2018-05-22 19:45:12 +0800

    添加列子

---
+++ src/components/project/projectMain.vue
@@ -0,0 +1,51 @@
+
+<template>
+    <div class="project-main">
+      <el-menu :default-active="onRoutes" class="el-menu-demo" mode="horizontal" @select="handleSelect" router>
+        <el-menu-item  v-for="(item, index) in itemList" :index="item.path" :key="index">{{item.name}}</el-menu-item>
+      </el-menu>
+      <router-view/>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "project-main",
+    data() {
+      return {
+        itemList:[{name:"项目1",path:"/project"},{name:"项目2",path:"/project/projectTwo"}],
+        activeIndex: "0"
+      }
+    },
+    computed: {
+      // 首次进入页面时展开当前页面所属的菜单
+      onRoutes(){
+        return this.$route.path
+        }
+      },
+    updated() {//路径及按键操作时调用
+      this.updateForcus();
+    },
+
+    mounted() {//载入时调用，F5刷新会起效
+      this.updateForcus();
+    },
+    created(){
+
+    },
+    methods: {
+      updateForcus() {
+        let path = this.$route.path;
+
+
+      },
+      handleSelect(key,keyPath){
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  @import "projectMain.less";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/helloWorld/helloWorld'
+import projectMain from '@/components/project/projectMain'
 
+const projectOne = () => import(/* webpackChunkName: "projectOne" */ '@/components/project/projectOne/projectOne')
+const projectTwo = () => import(/* webpackChunkName: "projectTwo" */ '@/components/project/projectTwo/projectTwo')
+const helloWorld = () => import(/* webpackChunkName: "projectOne" */ '@/components/project/projectOne/helloWorld/helloWorld')
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/project',
+      name: "projectMain",
+      component: projectMain,
+      children: [
+        {
+          path: '/',
+          name: "projectOne",
+          component: projectOne,
+          children: [
+            {
+              path: 'helloWorld',
+              name: "helloWorld",
+              component: helloWorld,
+            }
+          ]
+        },
+        {
+          path: 'projectTwo',
+          name: "projectTwo",
+          component: projectTwo,
+        }
+      ]
     }
   ]
 })
