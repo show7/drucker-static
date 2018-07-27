beb4ce24e5c7a102a4a0e726c2edfcef47f176e0
lance <451230805@qq.com>
2018-07-27 22:31:09 +0800

    小程序后台

--- config/index.js
+++ config/index.js
@@ -101,7 +101,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/drucker-static/',
+    assetsPublicPath: '/drucker_js/',
 
     /**
      * Source Maps
