24385d7ed6d846c0aa0897129be939b835faad7b
lance <451230805@qq.com>
2018-08-16 19:41:37 +0800

    提交

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ module.exports = {
         }
       },
       '/pc/*': {
-        target: 'http://192.168.1.148:8080',
+        target: 'http://beta.confucius.mobi',
         secure: false,
         changeOrigin: true,
         bypass: function (req) {
