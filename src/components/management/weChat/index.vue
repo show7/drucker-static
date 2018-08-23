15bdc94d9253c02444fad879f1f66741995e82fc
lance <451230805@qq.com>
2018-08-23 17:33:53 +0800

    提交

--- src/components/management/weChat/index.vue
+++ src/components/management/weChat/index.vue
@@ -19,7 +19,7 @@
             <el-menu-item index="/management/wechat/group/content">群内容管理</el-menu-item>
             <el-menu-item index="/management/wechat/admin/groups">群管理</el-menu-item>
             <el-menu-item index="/management/wechat/group/statistics">社群数据</el-menu-item>
-            <el-menu-item index="/management/wechat/community/topic">话题管理</el-menu-item>
+           <!-- <el-menu-item index="/management/wechat/community/topic">话题管理</el-menu-item>-->
            <!-- <el-menu-item index="/management/wechat/admin/contentmanage">内容管理</el-menu-item>-->
           </el-menu-item-group>
         </el-submenu>
