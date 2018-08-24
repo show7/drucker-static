b6acf5465bf02625c4499dedd727fba88eeee570
lance <451230805@qq.com>
2018-08-24 16:42:53 +0800

    提交

--- src/router/weChat.js
+++ src/router/weChat.js
@@ -9,7 +9,7 @@ const content = () => import(/* webpackChunkName: "wechat" */ '@/components/mana
 const groups = () => import(/* webpackChunkName: "wechat" */ '@/components/management/weChat/admin/groups/groups') //群管理
 const statistics = () => import(/* webpackChunkName: "wechat" */'@/components/management/weChat/group/statistics/statistics') //群组数据
 const topic = ()=> import(/* webpackChunkName: "wechat" */'@/components/management/weChat/community/topic/topic') //话题管理
-const contentManage = ()=> import(/* webpackChunkName: "wechat" */'@/components/management/weChat/admin/contentManage/contentManage') //内容管理
+
 
 
 export default [
@@ -17,5 +17,4 @@ export default [
   { path: 'admin/groups', name: 'group', component: groups },
   { path: 'group/statistics', name: 'statistics', component: statistics },
   { path: 'community/topic', name: 'topic', component: topic },
-  { path: 'admin/contentmanage', name: 'contentManage', component: contentManage },
 ]
