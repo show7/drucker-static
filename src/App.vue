70a0f822f542b30f3a61753d9ed5e59255a255be
lance <451230805@qq.com>
2018-12-04 15:11:28 +0800

    添加

--- src/App.vue
+++ src/App.vue
@@ -9,26 +9,9 @@ import apiDataFilter from "./libraries/apiDataFilter";
 
 export default {
   name: 'App',
-  methods:{
-    getCustomer(){
-      window.ENV ={
-
-      }
-      apiDataFilter.request({
-        apiPath:'common.info',
-        successCallback:(res)=>{
-          window.ENV.userName = res.msg.nickname;
-          window.ENV.headImgUrl = res.msg.headimgurl;
-          window.ENV.riseId = res.msg.riseId;
-          window.ENV.isAsst = res.msg.isAsst;
-          window.ENV.roleNames = res.msg.roleNames;
-          window.ENV.classGroupMaps = res.msg.classGroupMaps;
-        }
-      })
-    }
-  },
+  methods:{},
   created(){
-    this.getCustomer();
+
   }
 }
 </script>
