41ddea234d80c96b57257e23a8bb57b86e5b509a
BengBu-YueZhang <zhangyue_coder@163.com>
2019-01-18 19:56:27 +0800

    💎【Add】添加搜索的功能

--- src/components/management/course/courseThinking/courseThinking.vue
+++ src/components/management/course/courseThinking/courseThinking.vue
@@ -5,7 +5,7 @@
       <el-row>
         <el-col :span="12">
           <h4>课程</h4>
-          <el-select v-model="searchCourseTitleValueId" placeholder="请选择小课标题" @change="handleChangeSelect">
+          <el-select v-model="searchCourseTitleValueId" filterable placeholder="请选择小课标题" @change="handleChangeSelect">
             <el-option
               v-for="item in courseTitleList"
               :key="item.id"
@@ -76,7 +76,7 @@
         <el-row>
           <el-col :span="12">
             <h4>课程</h4>
-            <el-select v-model="courseTitleValueId" :disabled="disabledFlag" placeholder="请选择小课标题">
+            <el-select v-model="courseTitleValueId" filterable :disabled="disabledFlag" placeholder="请选择小课标题">
               <el-option
                 v-for="item in courseTitleList"
                 :key="item.id"
