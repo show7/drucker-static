7f8ce0ff55883dd5fd9250d1a2fa1ba709f4df7e
BengBu-YueZhang <zhangyue_coder@163.com>
2019-01-22 14:16:02 +0800

    💎【Add】添加任务保后台的模块

---
+++ src/components/management/manage/task/components/dialog.vue
@@ -0,0 +1,38 @@
+<template>
+<el-dialog title="编辑任务" :visible="dialogVisible">
+  <div slot="footer" class="dialog-footer">
+    <el-button type="primary" @click="handleSave">保 存</el-button>
+  </div>
+</el-dialog>
+</template>
+
+<script>
+import Steps from './steps'
+
+export default {
+
+  components: {
+    Steps
+  },
+  
+  props: {
+    dialogVisible: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+
+  methods: {
+    /**
+     * 保存按钮点击
+     */
+    handleSave () {
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import url('./dialog.less');
+</style>
---
+++ src/components/management/manage/task/task.vue
@@ -0,0 +1,45 @@
+<template>
+<section class="wrapper">
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="ActivityChineseName" label="推广活动名"></el-table-column>
+    <el-table-column label="编辑">
+      <template slot-scope="scope">
+        <el-button @click="handleEdit(scope.row)">编辑</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+  <Dialog
+    :dialogVisible="dialogVisible"
+  />
+</section>
+</template>
+
+<script>
+import Dialog from './components/dialog'
+
+export default {
+
+  components: {
+    Dialog
+  },
+
+  data() {
+    return {
+      tableData: [],
+      dialogVisible: true
+    };
+  },
+
+  methods: {
+    /**
+     * 编辑按钮点击
+     */
+    handleEdit (row) {
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+@import url('./task.less');
+</style>
