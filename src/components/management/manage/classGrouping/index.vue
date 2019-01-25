d10a116e96839af18a5929f1f2dd71a6483e7c50
BengBu-YueZhang <zhangyue_coder@163.com>
2019-01-25 15:47:32 +0800

    对接上传接口

--- src/components/management/manage/classGrouping/index.vue
+++ src/components/management/manage/classGrouping/index.vue
@@ -1,11 +1,42 @@
 <template>
+<div class="class-grouping-container">
+  <h3>班级分组导入</h3>
+  <div class="class-grouping-top">
+    <el-upload
+      class="upload-demo"
+      action="/pc/operation/social/group/upload"
+      :show-file-list="false"
+      :before-upload="beforeUpload"
+      :on-success="handleSuccess">
+      <el-button size="small" type="primary">上传EXCEL</el-button>
+      <div slot="tip" class="el-upload__tip">只能上传.xls文件</div>
+    </el-upload>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
+  name: 'social',
+  methods: {
+    handleSuccess (res, file) {
+      this.$message.success('上传成功')
+    },
+    beforeUpload (file) {
+      let typeName = file.name.substring(file.name.lastIndexOf('.') + 1).toLowerCase();
+      const isXLS = typeName === 'xls';
+      if (!isXLS) {
+        this.$message.error('只能上传.xls格式!');
+      }
+      return isXLS;
+    }
+  },
+  created () {
+
+  }
 }
 </script>
 
-<style lang="less" scoped>
+<style scoped lang="less">
 @import url('./index.less');
 </style>
