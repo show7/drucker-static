220264214ac53e5aaaad4e7282dd3a9654a75e1f
minry <minry@iquanwai.com>
2019-05-10 19:20:09 +0800

    运营后台添加强制关闭课程的功能

--- src/components/management/manage/webWeChatPublic/publicCheck/publicCheck.vue
+++ src/components/management/manage/webWeChatPublic/publicCheck/publicCheck.vue
@@ -60,6 +60,7 @@
         class="avatar-uploader"
         action="/pc/upload/file"
         :show-file-list="false"
+        :before-upload="beforeAvatarUpload"
         :on-success="handleAvatarSuccess">
         <img v-if="avatar" :src="avatar" class="avatar">
         <i v-else class="el-icon-plus avatar-uploader-icon"></i>
@@ -139,6 +140,18 @@
       handleAvatarSuccess(res, file) {
         this.avatar=res.msg;
         this.imageUrl = res.msg;
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = (file.type === 'image/jpeg')||(file.type === 'image/png');
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 或 PNG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
       }
     }
   };
